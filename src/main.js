4d13c40dd10fa9cbaaab09ceadbab2e5a120f39c
Leon Kurvers <kurvers.leon@gmail.com>
2023-01-30 16:09:58 +0100

    styling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// Mobile navigation
 const primaryHeader = document.querySelector('.primary-header')
 const navToggle = document.querySelector(".mobile-nav-toggle")
 const primaryNavigation = document.querySelector('.primary-navigation')
@@ -15,10 +16,22 @@ navToggle.addEventListener('click', function() {
     }
 }) 
 
-
+// Shopping cart
 const cartButton = document.querySelector('.cart-button')
 const shoppingCart = document.querySelector('.cart')
 
 cartButton.addEventListener('click', function() {
     shoppingCart.toggleAttribute('hidden')
+})
+
+// Mobile slider
+const previous = document.querySelector('.prev')
+const next = document.querySelector('next')
+
+let slides = document.querySelectorAll('.img_big')
+
+next.addEventListener('click', function() {
+    for (i = 0, i < slides.length, i++) {
+        console.log(slides)
+    }
 })
